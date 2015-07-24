b3cb4dc219e7a88273420dcc6d6cdf9352bcea51
Xin Wang <wangamanda.lu@gmail.com>
2015-07-24 10:34:15 -0400

    add readme

---
+++ DecToHex/Readme.txt
@@ -0,0 +1,24 @@
+*Requirement:
+Linux, SDK Android 22, Java 7
+
+*What are included in this project:
+
+1. A simple program to convert an Integer 32bits input to a hexadecimal String representation (ConvertDec.java). 
+
+2. Build an android program UI with a text field input and a button(MyActivity.java).
+
+3. The text field will be used to input the integer. The button will be used to call and handle the process to convert.
+
+4. The result will be displayed in a pop up and via log simultaneously. 
+
+*How to use:
+
+run the bash script make.sh, it will 
+
+1. generate R.java file
+
+2. compile and generate dex files
+
+3. generate apk file
+
+4. generate java docs
--- DecToHex/make.sh
+++ DecToHex/make.sh
@@ -7,21 +7,25 @@ export ANDROID_HOME=/home/xin/android-sdk-linux/
 export PATH=$PATH:$ANDROID_HOME/build-tools/22.0.1:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
 
 #create R.java file
-aapt package -v -f -m -S res/ -J src/ -M AndroidManifest.xml -I $ANDROID_HOME/platforms/android-22/android.jar
+aapt package  -f -m -S res/ -J src/ -M AndroidManifest.xml -I $ANDROID_HOME/platforms/android-22/android.jar
 
 #compile all .java files
-javac -verbose -d obj -classpath $ANDROID_HOME/platforms/android-22/android.jar:./obj/ -sourcepath ./src/ ./src/com/convert/package1/*.java
+javac -d obj -classpath $ANDROID_HOME/platforms/android-22/android.jar:./obj/ -sourcepath ./src/ ./src/com/convert/package1/*.java
 
 #generate .dex file
-dx --dex --verbose --output=bin/classes.dex obj/ lib/
+dx --dex  --output=bin/classes.dex obj/ lib/ > /dev/null
 
 #generate unsigned apk
-aapt package -v -f -M ./AndroidManifest.xml -S ./res/ -I $ANDROID_HOME/platforms/android-22/android.jar -F ./bin/Converter.unsigned.apk ./bin/
+aapt package  -f -M ./AndroidManifest.xml -S ./res/ -I $ANDROID_HOME/platforms/android-22/android.jar -F ./bin/Converter.unsigned.apk ./bin/ > /dev/null
 
 #generate key
-keytool -genkeypair -validity 10000 -dname "CN=Angela, OU=Android, L=bethlehem, S=PA, C=01" -keystore Converter.keystore -storepass 123456 -keypass 123456 -alias ConverterKey -keyalg RSA -v
+keytool -genkeypair -validity 10000 -dname "CN=Angela, OU=Android, L=bethlehem, S=PA, C=01" -keystore Converter.keystore -storepass 123456 -keypass 123456 -alias ConverterKey -keyalg RSA > /dev/null
 
 #sign apk using key
-jarsigner -verbose -keystore ./Converter.keystore -storepass 123456 -keypass 123456 -signedjar ./bin/Converter.signed.apk ./bin/Converter.unsigned.apk ConverterKey
+jarsigner -keystore ./Converter.keystore -storepass 123456 -keypass 123456 -signedjar ./bin/Converter.signed.apk ./bin/Converter.unsigned.apk ConverterKey > /dev/null 2> /dev/null
 
-zipalign -v -f 4 ./bin/Converter.signed.apk ./bin/Converter.apk
+zipalign  -f 4 ./bin/Converter.signed.apk ./bin/Converter.apk
+
+#generate java docs
+javadoc  -d docs -sourcepath src/ -classpath $ANDROID_HOME/platforms/android-22/android.jar:./obj/ -author -package -use -splitIndex -version -windowtitle 'Converter' -doctitle 'Converter' ./src/com/convert/package1/*.java > /dev/null
+
